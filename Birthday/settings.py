55b2a1f7ebe963b70b96a511e63716e5b1c7c6f7
Pranav Rajendraprasad <pranavprasad.r500@gmail.com>
2025-01-13 22:41:37 +0530

    Changed database from mysql to sqlite3

--- Birthday/settings.py
+++ Birthday/settings.py
@@ -75,13 +75,17 @@ WSGI_APPLICATION = 'Birthday.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'Your database name',
+    #     'USER': 'Your mysql username',
+    #     'PASSWORD': 'Your mysql password',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'birthday',
-        'USER': 'root',
-        'PASSWORD': '7A8b9c.123',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
